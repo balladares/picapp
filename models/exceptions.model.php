43ee6d9ef35326d1ecb58f16c9e7997e2a78b69e
balladares <pozodicas@gmail.com>
2016-04-21 19:45:14 -0300

    Fix erro exceptions

---
+++ models/exceptions.model.php
@@ -0,0 +1,12 @@
+<?php
+ /**
+ * PicApp 2016
+ * Exceptions class
+ * Funções para manipulação das mensagens de retorno
+ * Gabriel Balladares
+ */
+
+ class PicAppError extends Exceptions
+ {
+  
+ }
